c3f0875d214387f76c25ef503f411e299764c6dc
Long Pham Duc <long.phamduc@euroland.com>
2023-05-05 09:04:47 +0700

    save code

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { init as initHomePage } from './home-page';
 import { init as initLoginPage } from './login-page';
 import { init as initIframePage } from './iframe-page';
 import { init as initNewSolutionPage } from './new-solution-page';
-import { init as initLovePage } from './love-page'
 
 switch(location.pathname) {
   case '/': {
@@ -19,10 +18,6 @@ switch(location.pathname) {
   }
   case '/new-solution': {
     initNewSolutionPage();
-    break;
-  }
-  case '/love': {
-    initLovePage();
     break;
   }
   default: {
